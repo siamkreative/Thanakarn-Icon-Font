7ce05ffb4202237910046c6477474d1ccb7d204b
Julien Vernet <julien@vernet.me>
2016-12-02 13:42:08 +0700

    Fix indentation

--- gulpfile.js
+++ gulpfile.js
@@ -62,12 +62,12 @@ gulp.task('genFontsCSS', function (done) {
 });
 
 gulp.task('serve', function() {
-    browserSync.init({
-        server: {
-            baseDir: './'
-        }
-    });
-    gulp.watch('index.html').on('change', browserSync.reload);
+	browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});
+	gulp.watch('index.html').on('change', browserSync.reload);
 });
 
 gulp.task('default', [], function () {
